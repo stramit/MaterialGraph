8e0b83f35274c1f166003ced1ba46cf48174ebfd
Keijiro Takahashi <keijiro@gmail.com>
2018-03-02 17:36:44 +0900

    Add "Generated code" item to the shader graph inspector.
    Added "Generated code" item to the shader graph inspector. It has a drop down
    list to select generation mode (previews or for-reals) and "View" button that
    opens an external code editor with generation results. These features allow
    users to inspect generated code with the shader graph.

--- com.unity.shadergraph/Editor/Importers/ShaderGraphImporterEditor.cs
+++ com.unity.shadergraph/Editor/Importers/ShaderGraphImporterEditor.cs
@@ -1,21 +1,34 @@
+using System.Collections.Generic;
 using System.IO;
 using UnityEditor;
 using UnityEditor.Callbacks;
 using UnityEditor.Experimental.AssetImporters;
+using UnityEditor.ShaderGraph;
 using UnityEditor.ShaderGraph.Drawing;
 using UnityEngine;
 
 [CustomEditor(typeof(ShaderGraphImporter))]
 public class ShaderGraphImporterEditor : ScriptedImporterEditor
 {
+    static GenerationMode s_DebugGenerationMode = GenerationMode.ForReals;
+
     public override void OnInspectorGUI()
     {
+        AssetImporter importer = target as AssetImporter;
+
         if (GUILayout.Button("Open Shader Editor"))
         {
-            AssetImporter importer = target as AssetImporter;
             Debug.Assert(importer != null, "importer != null");
             ShowGraphEditWindow(importer.assetPath);
         }
+
+        EditorGUILayout.Space();
+
+        EditorGUILayout.BeginHorizontal();
+        s_DebugGenerationMode = (GenerationMode)EditorGUILayout.EnumPopup("Generated code", s_DebugGenerationMode);
+        if (GUILayout.Button("View", EditorStyles.miniButton, GUILayout.ExpandWidth(false)))
+            ShowGeneratedShader(importer.assetPath, s_DebugGenerationMode);
+        EditorGUILayout.EndHorizontal();
     }
 
     internal static bool ShowGraphEditWindow(string path)
@@ -42,6 +55,24 @@ public class ShaderGraphImporterEditor : ScriptedImporterEditor
             window.Initialize(guid);
         }
         return true;
+    }
+
+    internal static void ShowGeneratedShader(string path, GenerationMode generationMode)
+    {
+        var name = Path.GetFileNameWithoutExtension(path);
+
+        var json = File.ReadAllText(path, System.Text.Encoding.UTF8);
+        var graph = JsonUtility.FromJson<MaterialGraph>(json);
+        graph.LoadedFromDisk();
+
+        List<PropertyCollector.TextureInfo> textures;
+        var shaderName = string.Format("graphs/{0}", name);
+        var shaderString = graph.GetShader(shaderName, generationMode, out textures);
+
+        var tempPath = string.Format("Temp/GeneratedShader-{0}-{1}.shader", name, generationMode);
+        File.WriteAllText(tempPath, shaderString);
+
+        UnityEditorInternal.InternalEditorUtility.OpenFileAtLineExternal(tempPath, 0);
     }
 
     [OnOpenAsset(0)]
